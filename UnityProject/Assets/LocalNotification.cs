8d11d7fceec988294d27003613798abde476b714
ya.parshutin <ya.parshutin@corp.nekki.ru>
2016-04-14 19:01:22 +0300

    обновил менеджер

--- UnityProject/Assets/LocalNotification.cs
+++ UnityProject/Assets/LocalNotification.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System;
-using CalendarUnit = UnityEngine.iOS.CalendarUnit;
-using NotificationServices = UnityEngine.iOS.NotificationServices;
-using LocalNotification = UnityEngine.iOS.LocalNotification;
-
+using System.Collections.Generic;
 
 public class LocalNotificationManager
 {
+    private const string AndroidClassName = "net.agasper.unitynotification.UnityNotificationManager";
+
+    private const string AndroidMainActivityClassName = "com.unity3d.player.UnityPlayerNativeActivity";
+
     private static LocalNotificationManager _Instance;
 
     public static LocalNotificationManager Instance
@@ -15,6 +16,9 @@ public class LocalNotificationManager
         {
             if (_Instance == null)
             {
+#if UNITY_IOS
+                NotificationServices.RegisterForNotifications(NotificationType.Alert | NotificationType.Badge);
+#endif
                 _Instance = new LocalNotificationManager();
             }
 
@@ -22,6 +26,82 @@ public class LocalNotificationManager
         }
     }
 
+#if UNITY_IOS
+    public void CreateIOSNotification(int id,string title, string message, TimeSpan delay, UnityEngine.iOS.CalendarUnit repeatInterval = UnityEngine.iOS.CalendarUnit.Era)
+    {
+        var notif = new UnityEngine.iOS.LocalNotification();
+        notif.userInfo = new Dictionary<string, int>();
+        notif.userInfo["id"] = id;
+        notif.alertAction = title;
+        notif.fireDate = DateTime.Now.AddSeconds(delay.TotalSeconds);
+        notif.alertBody = message;
+        notif.repeatInterval = repeatInterval;
+        notif.soundName = UnityEngine.iOS.LocalNotification.defaultSoundName;
+        UnityEngine.iOS.NotificationServices.ScheduleLocalNotification(notif);
+    }
+
+    public void CancelIOSNotification(int id)
+    {
+        foreach (var notification in UnityEngine.iOS.NotificationServices.localNotifications)
+        {
+            if ((int)notification.userInfo["id"] == id)
+            {
+                UnityEngine.iOS.NotificationServices.CancelLocalNotification(notification);
+            }
+        }
+    }
+
+    public void CancelAllIOSNotifications()
+    {
+        UnityEngine.iOS.NotificationServices.CancelAllLocalNotifications();
+    }
+#endif
+
+    #if UNITY_ANDROID
+
+    public void CreateAndroidNotification(int id, string title, string message, TimeSpan delay)
+    {
+        AndroidJavaClass pluginClass = new AndroidJavaClass(AndroidClassName);
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("SetNotification", id, delay.TotalSeconds*1000L, title, message, message, 1, 1, 1,
+                0, AndroidMainActivityClassName);
+        }
+    }
+
+    public void CreateAndroidRepeatingNotification(int id, string title, string message, TimeSpan delay,
+        TimeSpan timeout)
+    {
+        AndroidJavaClass pluginClass = new AndroidJavaClass(AndroidClassName);
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("SetRepeatingNotification", id, delay.TotalSeconds*1000L, title, message, message,
+                timeout.TotalSeconds * 1000L, 1, 1, 1, 0, AndroidMainActivityClassName);
+        }
+    }
+
+    public void CancelAndroidNotification(int id)
+    {
+        AndroidJavaClass pluginClass = new AndroidJavaClass(AndroidClassName);
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("CancelNotification", id);
+        }
+    }
+
+
+
+    public void CancelAllAndroidNotifications()
+    {
+        AndroidJavaClass pluginClass = new AndroidJavaClass(AndroidClassName);
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("CancelAll");
+        }
+    }
+#endif
+
+    /*
     public enum NotificationExecuteMode
     {
         Inexact = 0,
@@ -29,10 +109,10 @@ public class LocalNotificationManager
         ExactAndAllowWhileIdle = 2
     }
 
-#if UNITY_ANDROID && !UNITY_EDITOR
+
     private static string fullClassName = "net.agasper.unitynotification.UnityNotificationManager";
     private static string mainActivityClassName = "com.unity3d.player.UnityPlayerNativeActivity";
-#endif
+
 
     public static void SendNotification(int id, TimeSpan delay, string title, string message)
     {
@@ -49,12 +129,7 @@ public class LocalNotificationManager
         }
 #elif UNITY_IOS && !UNITY_EDITOR
 
-        var notif = new LocalNotification();
-        notif.alertAction = title;
-        notif.fireDate = DateTime.Now.AddSeconds(delay);
-        notif.alertBody = message;
-        NotificationServices.ScheduleLocalNotification(notif);
-        NotificationTest.Text = "SendNotification";
+       
 #endif
     }
 
@@ -101,5 +176,5 @@ public class LocalNotificationManager
 #elif UNITY_IOS && !UNITY_EDITOR
         NotificationServices.CancelAllLocalNotifications();
 #endif
-    }
+    }*/
 }
--- UnityProject/Assets/NotificationTest.cs
+++ UnityProject/Assets/NotificationTest.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.iOS;
-using NotificationServices = UnityEngine.iOS.NotificationServices;
-using LocalNotification = UnityEngine.iOS.LocalNotification;
+
 public class NotificationTest : MonoBehaviour {
 
     float sleepUntil = 0;
     public static string Text = "";
+
+    private int count = 0;
 
     private void Start()
     {
@@ -20,35 +22,54 @@ public class NotificationTest : MonoBehaviour {
         GUI.Label(new Rect(500, 250, 400, 200), Text);
         if (GUILayout.Button("5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
         {
-            LocalNotificationManager.SendNotification(1, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
+            #if UNITY_ANDROID
+                LocalNotificationManager.Instance.CreateAndroidNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(5));
+            #elif UNITY_IOS
+                LocalNotificationManager.Instance.CreateIOSNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(5));
+            #endif
+            count++;
             sleepUntil = Time.time + 5;
         }
 
-        if (GUILayout.Button("5 SECONDS BIG ICON", GUILayout.Height(Screen.height * 0.2f)))
+        if (GUILayout.Button("30 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
         {
-            LocalNotificationManager.SendNotification(1, 5, "Title", "Long message text with big icon", new Color32(0xff, 0x44, 0x44, 255), true, true, true, "app_icon");
-            sleepUntil = Time.time + 5;
+#if UNITY_ANDROID
+                LocalNotificationManager.Instance.CreateAndroidNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(30));
+#elif UNITY_IOS
+                LocalNotificationManager.Instance.CreateIOSNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(30));
+#endif
+            sleepUntil = Time.time + 30;
+            count++;
         }
 
-        if (GUILayout.Button("EVERY 5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
+        if (GUILayout.Button("EVERY 60 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
         {
-            LocalNotificationManager.SendRepeatingNotification(1, 5, 5, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
+#if UNITY_ANDROID
+                LocalNotificationManager.Instance.CreateAndroidRepeatingNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(60));
+#elif UNITY_IOS
+                LocalNotificationManager.Instance.CreateIOSNotification(count, "Title", "Long message text", TimeSpan.FromSeconds(5), UnityEngine.iOS.CalendarUnit.Minute);
+#endif
             sleepUntil = Time.time + 99999;
-        }
-
-        if (GUILayout.Button("10 SECONDS EXACT", GUILayout.Height(Screen.height * 0.2f)))
-        {
-            LocalNotificationManager.SendNotification(1, 10, "Title", "Long exact message text", new Color32(0xff, 0x44, 0x44, 255), executeMode: LocalNotificationManager.NotificationExecuteMode.ExactAndAllowWhileIdle);
-            sleepUntil = Time.time + 10;
         }
 
         GUI.enabled = true;
 
         if (GUILayout.Button("STOP", GUILayout.Height(Screen.height * 0.2f)))
         {
-            LocalNotificationManager.CancelNotification(1);
-            sleepUntil = 0;
+#if UNITY_ANDROID
+            LocalNotificationManager.Instance.CancelAndroidNotification(count);
+#elif UNITY_IOS
+            LocalNotificationManager.Instance.CancelIOSNotification(count);
+#endif
         }
-        
+
+        if (GUILayout.Button("STOP ALL", GUILayout.Height(Screen.height * 0.2f)))
+        {
+#if UNITY_ANDROID
+            LocalNotificationManager.Instance.CancelAllAndroidNotifications();
+#elif UNITY_IOS
+            LocalNotificationManager.Instance.CancelAllIOSNotifications();
+#endif
+        }
 	}
 }
